a97cf7cb505e1c7b6a48e1ae03dd8632e6b62d1c
Ainur <ainur.khasanv@gmail.com>
2025-12-05 01:41:19 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int Balance = 100; // Начальный баланс
+        int deposit = 1100;// Сумма пополнения
+        int bonus = 0;
+
+        if (deposit > 1000){
+            bonus = deposit/100;
+        }
+        int finalbonus = Balance +deposit+bonus;
+        System.out.println("Начальный баланс: " + Balance + " Рублей");
+        System.out.println("Пополнено на: " + deposit + " рублей");
+        System.out.println("Начислено бонусов: " + bonus + " Рублей");
+        System.out.println("Итого: "+ finalbonus + " Рублей");
+
+    }
+}
