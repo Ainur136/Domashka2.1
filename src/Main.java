16994026957f0bf9a9e6cd1affe8fc13445806c6
Ainur <ainur.khasanv@gmail.com>
2025-12-07 23:29:16 +0500

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,14 @@ public class Main {
         int deposit = 1100;// Сумма пополнения
         int bonus = 0;
 
-        if (deposit > 1000){
-            bonus = deposit/100;
+        if (deposit > 1000) {
+            bonus = deposit / 100;
         }
-        int finalbonus = balance +deposit+bonus;
+        int finalBonus = balance + deposit + bonus;
         System.out.println("Начальный баланс: " + balance + " Рублей");
         System.out.println("Пополнено на: " + deposit + " рублей");
         System.out.println("Начислено бонусов: " + bonus + " Рублей");
-        System.out.println("Итого: "+ finalbonus + " Рублей");
+        System.out.println("Итого: " + finalBonus + " Рублей");
 
     }
 }
