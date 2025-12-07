fb1f6cf9b54c83b5596aa3f75851da3a135faf05
Ainur <ainur.khasanv@gmail.com>
2025-12-07 20:34:05 +0500

    commit #2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        int Balance = 100; // Начальный баланс
+        int balance = 100; // Начальный баланс
         int deposit = 1100;// Сумма пополнения
         int bonus = 0;
 
         if (deposit > 1000){
             bonus = deposit/100;
         }
-        int finalbonus = Balance +deposit+bonus;
-        System.out.println("Начальный баланс: " + Balance + " Рублей");
+        int finalbonus = balance +deposit+bonus;
+        System.out.println("Начальный баланс: " + balance + " Рублей");
         System.out.println("Пополнено на: " + deposit + " рублей");
         System.out.println("Начислено бонусов: " + bonus + " Рублей");
         System.out.println("Итого: "+ finalbonus + " Рублей");
